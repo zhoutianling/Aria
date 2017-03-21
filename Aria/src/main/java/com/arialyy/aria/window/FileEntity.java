eee6f45471a522c3334e7b492520bcfdb4b5df48
AriaLyy <511455842@qq.com>
2017-03-21 18:11:51 +0800

    文件夹

---
+++ Aria/src/main/java/com/arialyy/aria/window/FileEntity.java
@@ -0,0 +1,14 @@
+package com.arialyy.aria.window;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by Aria.Lao on 2017/3/21.
+ */
+
+public class FileEntity {
+  public String fileName;
+  public String fileInfo;
+  public int fileIcon;
+  public Drawable fileDrawable;
+}
